0886c3d829449581fadaefec853d3e22e4af0e0c
nath-techie <64378408+nath-techie@users.noreply.github.com>
2022-06-15 02:28:14 +0530

    added resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import resume from "./pics/SABARIA.pdf"
 function Contact(){
   return(<div className="contact-page">
     <h1>Contact</h1>
@@ -5,7 +6,7 @@ function Contact(){
      <h2>
     <a href="mailto:nathansabari2020@gmail.com?subject=Hi Sabari ,Saw your Portfolio">Email me</a></h2>
     <h2>Phone :+91 7010026830</h2>
-
+<a href={resume} download="Sabari Resume" className="btn">Download my Resume</a>
     </div>
   );
 }
