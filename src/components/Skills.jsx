a857174047e1b70908cfaf9553df7e0ccc6385b5
Sabari Nathan <64378408+nath-techie@users.noreply.github.com>
2023-09-17 02:18:22 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import filler1 from "./pics/f1.png";
 
 function Skills(){
   return(<div className="skill-page"  id="2">
-    <img src={filler1} alt="Icon description" class="filler-images">
+    <img src={filler1} alt="Icon description" class="filler-images"/>
   <img src={html}className="skill" alt="front end skills"/>
  
     </div>
