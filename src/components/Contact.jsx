17ee7d4d5cfe30abeee4df89cf93444c2d3d18de
Sabari Nathan <64378408+nath-techie@users.noreply.github.com>
2025-10-22 17:04:59 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,10 @@
-import resume from "./pics/Sabari Nathan Resume.pdf"
 import w from "./pics/whatsapp.png"
 import g from "./pics/gmail.png"
 
 function Contact(){
   return(<div className="contact-page"  id="4">
 <span>Contact me at:</span>
-   <span>
-    <a href="mailto:nathansabari2020@gmail.com?subject=Hi Sabari ,Saw your Portfolio" target="_blank">
-    <img src={g} className="contact-icons" alt="whatsapp"/>
-    </a>
    
-    </span>
-
 
 
 <br />
